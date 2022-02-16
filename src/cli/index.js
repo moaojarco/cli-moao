58c8ae9b15c83f15065e93fcb91d6fa486c78de5
João Marco <moaojarco@gmail.com>
2022-02-16 11:40:54 -0400

    Starting... 🚀

---
+++ src/cli/index.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+
+fs.basedir = "./src";
+
+const componentsFolder = "Components" || "components";
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function createFC() {
+  let componentName = readline.question(`Enter the component name: `, (name) => {
+    console.log(`Created ${name}`);
+    componentName = name;
+
+    if (fs.existsSync(`./src/${componentsFolder}/${componentName}.tsx`)) throw Error;
+
+    fs.writeFileSync(
+      `./src/components/${componentName}.tsx`,
+      `import { useState } from 'react';
+
+const ${componentName} = () => {
+    
+  const [initial, setInitial] = useState<string>("");
+
+  return (
+    <h1>Component</h1>
+  )
+}`
+    );
+    readline.close();
+  });
+}
+
+function deleteFC() {
+  let componentName = readline.question(`Component to delete: `, (name) => {
+    componentName = name;
+    let componentPath = `./src/components/${name}.tsx`;
+
+    fs.unlink(componentPath, () => {
+      console.log("Component deleted!");
+    });
+
+    readline.close();
+  });
+}
+
+readline.question(`What you want to do? ('create' or 'delete') `, (res) => {
+  if (res.includes("create")) createFC();
+  if (res.includes("delete")) deleteFC();
+});
