3a59340c65ab0636b250c2b40ef0771a485803af
João Marco <moaojarco@gmail.com>
2022-02-16 20:38:50 -0400

    0.1.2

--- components/Layout/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { styles } from "./styles.module.scss";
-
-const Layout = () => {
-  const [initial, setInitial] = useState<string>("");
-
-  return <h1>Component</h1>;
-};
